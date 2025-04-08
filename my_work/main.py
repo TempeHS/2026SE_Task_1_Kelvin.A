a6adfadf7d5041d394f8a2b93609b94da2d2b3f4
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-04-08 04:32:01 +0000

    work on register

--- my_work/main.py
+++ my_work/main.py
@@ -1,27 +1,42 @@
 import csv
-import sys
 
+## Input user for 1,2,3 options
 user = input('Enter a num: 1. Login, 2. Register, 3. Quit: ')
 
 logins = []
-detail = []
 with open('details.csv') as details:
     reader = csv.DictReader(details)
     for row in reader:
         logins.append({'user': row['user'], 'password': row['password']})
 
 if user == '1':
-    details = input("what is your user and password? ")
+    try:
+        detail = input('what is up (u,p): ')
+        a = detail.split(',')
+        user,password = a[0], a[1]
+        simple = user,password
+        if simple in logins:
+            print('logged in')
+        else:
+            print('boo')
+    except IndexError:
+        print('in format of "username,password"')
+        
+
+
+if user == '2':
+    new = input("new username and password? (A,B): ").split(',')
+    user_2,password_2 = new[0], new[1]
+    userS = new[0], new[1]
+    print(userS)
 
 
 
 
 
-elif user == '2':
 
-elif user == '3':
 
-else:
+
 
 
 
