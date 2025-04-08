b32238294ade64ec695659e9d216b661b9e125d9
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-04-08 04:15:36 +0000

    add flowchart

--- my_work/main.py
+++ my_work/main.py
@@ -5,12 +5,26 @@ user = input('Enter a num: 1. Login, 2. Register, 3. Quit: ')
 
 logins = []
 detail = []
+with open('details.csv') as details:
+    reader = csv.DictReader(details)
+    for row in reader:
+        logins.append({'user': row['user'], 'password': row['password']})
 
 if user == '1':
-    with open('details.csv') as details:
-        reader = csv.DictReader(details)
-        for row in reader:
-            logins.append({'user': row['user'], 'password': row['password']})
+    details = input("what is your user and password? ")
+
+
+
+
+
+elif user == '2':
+
+elif user == '3':
+
+else:
+
+
+
 
 
 
